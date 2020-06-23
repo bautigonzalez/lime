ffc4ea2b491104cae301f37dc49076c3e2a3096d
bautigonzalez <62444708+bautigonzalez@users.noreply.github.com>
2020-06-23 17:26:57 -0300

    Update index.js

--- back/db/index.js
+++ back/db/index.js
@@ -1,6 +1,6 @@
  
 const Sequelize = require("sequelize");
-const db = new Sequelize("postgres://postgres:argentina09@localhost:5432/lime", {
+const db = new Sequelize("postgres://localhost:5432/lime", {
     logging: false
 });
 
